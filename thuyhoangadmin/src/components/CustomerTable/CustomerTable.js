16238f8af48b4ae7f37db22e3459b00583b67791
AidenTran15 <aidenkiettran@gmail.com>
2024-09-22 17:47:55 +1000

    Refactor CustomerTable component into a separate file and display in HomePage

---
+++ thuyhoangadmin/src/components/CustomerTable/CustomerTable.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './CustomerTable.css';  // Assuming you have the CSS in a separate file
+
+const CustomerTable = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://twnbtj6wuc.execute-api.ap-southeast-2.amazonaws.com/prod/customers')
+      .then(response => {
+        const customerData = JSON.parse(response.data.body);
+        setCustomers(Array.isArray(customerData) ? customerData : []);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the customers!", error);
+      });
+  }, []);
+
+  return (
+    <div className="customer-table">
+      <h2>Manage Customers</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+            <th>Pant Price</th>
+            <th>Shirt Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.length > 0 ? (
+            customers.map(customer => (
+              <tr key={customer['phone_number']}>
+                <td>{customer.name}</td>
+                <td>{customer['phone_number']}</td>
+                <td>{customer.address}</td>
+                <td>{customer.short_price}</td>
+                <td>{customer.dress_price}</td>
+                <td>
+                  <button>Block</button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="6">No customers found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CustomerTable;
--- thuyhoangadmin/src/components/HomePage/HomePage.js
+++ thuyhoangadmin/src/components/HomePage/HomePage.js
@@ -1,59 +1,13 @@
-import './HomePage.css'; 
+import React from 'react';
+import CustomerTable from '../CustomerTable/CustomerTable';  // Import the CustomerTable component
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-const CustomerTable = () => {
-  const [customers, setCustomers] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://twnbtj6wuc.execute-api.ap-southeast-2.amazonaws.com/prod/customers')
-      .then(response => {
-        const customerData = JSON.parse(response.data.body);
-        setCustomers(Array.isArray(customerData) ? customerData : []);
-      })
-      .catch(error => {
-        console.error("There was an error fetching the customers!", error);
-      });
-  }, []);
-
+const HomePage = () => {
   return (
-    <div className="customer-table">
-      <h2>Manage Customers</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Phone Number</th>
-            <th>Address</th>
-            <th>Pant Price</th>
-            <th>Shirt Price</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.length > 0 ? (
-            customers.map(customer => (
-              <tr key={customer['phone_number']}>
-                <td>{customer.name}</td>
-                <td>{customer['phone_number']}</td>
-                <td>{customer.address}</td>
-                <td>{customer.short_price}</td>
-                <td>{customer.dress_price}</td>
-                <td>
-                  <button>Block</button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="6">No customers found</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+    <div>
+      <h1>Home Page</h1>
+      <CustomerTable />  {/* Display the CustomerTable component */}
     </div>
   );
 };
 
-export default CustomerTable;
+export default HomePage;
